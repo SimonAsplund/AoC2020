c44eca80f0242dd5c1685bb5b279d7690351eb98
ASPLUND Simon <simon.asplund@leica-geosytems.com>
2020-12-01 07:40:26 +0100

    star one

--- 1/Day1.py
+++ 1/Day1.py
@@ -5,9 +5,18 @@ todaysInput1 = open("input1.txt", "r")
 print("Name of the file: ", todaysInput1.name)
 
 lines = todaysInput1.readlines()
+summan = 2020
+
+lines = [ int(l) for l in lines]
 
 for line in lines:
     # Do stuff
-    print(line)
+
+    diff = summan - line
+
+    if diff in lines:
+      print(line)
+      print(diff)
+      print(line * diff)
 # Close opened file
 todaysInput1.close()
